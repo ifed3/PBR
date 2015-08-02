3c287118b0b06c4b6df2deab74cbbb3d432a0e5d
strangerloops <hassinmichael@gmail.com>
2015-08-01 22:16:54 -0500

    cleanup

--- web_frontend/service.rb
+++ web_frontend/service.rb
@@ -7,7 +7,7 @@ def get_waypoints params
 
 	# why the frick do i need to divide by 10 lol
 	# decoded directions have lat/long off by factor of 10
-	# either mapzen's OSRM is buggy or this polylines library is buggy
+	# either mapzen's osrm is buggy or this polylines library is buggy
 	Polylines::Decoder.decode_polyline(directions['route_geometry']).map do |point|
 		point.map do |val|
 			val / 10
@@ -19,11 +19,11 @@ def geocode address
 	JSON.parse(get_url "http://pelias.mapzen.com/search?input=#{address}")['features'].first['geometry']['coordinates']
 end
 
-def get_url string
-	Net::HTTP.get(URI.parse(URI.encode(string)))
-end
-
 def get_directions origin, destination
 	# currently using mapzen's free osrm endpoint; point this to ours when we have it up
 	JSON.parse(get_url "http://osrm.mapzen.com/foot/viaroute?loc=#{origin.last},#{origin.first}&loc=#{destination.last},#{destination.first}&instructions=true")
+end
+
+def get_url string
+	Net::HTTP.get(URI.parse(URI.encode(string)))
 end

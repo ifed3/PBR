31e2846e9e7bae31e2f73616bca673686a8944b9
strangerloops <hassinmichael@gmail.com>
2015-08-15 20:08:37 -0400

    cleanup

--- street_closure/app.rb
+++ street_closure/app.rb
@@ -7,26 +7,26 @@ class ScrapeApp < Sinatra::Base
 	set :run, true
 	set :server, 'webrick'
 	
+  output_file = 'closure_coordinates.txt'
+
   get '/' do
-    'ping'    
+    'hi'    
   end
 
   get '/closures' do
-    send_file 'closure_coordinates.txt'
+    send_file output_file
   end
-
-  file_name = 'closure_coordinates.txt'
 
   seconds_per_day = 24 * 60 * 60
   interval = "#{seconds_per_day}s"
 
   scheduler = Rufus::Scheduler.new
   scheduler.every interval, :first_in => 0.1 do
-    File.open(file_name, 'w') do |file|
+    File.open(output_file, 'w') do |file|
       file.truncate 0
       file.puts "Last updated at: #{Time.now}"; file.puts
     end
-    scrape_street_closure_report file_name
+    scrape_street_closure_report output_file
   end
 
 	run! if app_file == $0
